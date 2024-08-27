6d6fdebe52b4725d8f64860a9966dae752c9f75a
tgabor343 <89416923+tgabor343@users.noreply.github.com>
2024-08-27 19:25:20 +0200

    datepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,8 +11,7 @@ import 'datatables.net-bs5';
 import 'datatables.net-buttons-bs5';
 import 'datatables.net-responsive-bs5';
 
-import 'jquery-ui';
-import 'jquery-ui/ui/widgets/datepicker.js';
+await import('jquery-ui/ui/widgets/datepicker.js');
 
 
 
