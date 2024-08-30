3de719ea09b19c1e676154694b39eb8c91ce1f56
tgabor343 <89416923+tgabor343@users.noreply.github.com>
2024-08-30 08:51:03 +0200

    fix

--- resources/js/borderdatemodal.js
+++ resources/js/borderdatemodal.js
@@ -1,13 +1,12 @@
-export function openBorderdateModal() {
-    const borderdateModalElement = $('.borderdate-modal');
-    borderdateModalElement.on('shown.bs.modal', (event) => {
-        $.ajax({
-            url: '/entity/modal/borderdate/' + event.target.dataset.type + '/' + event.target.dataset.id
-        }).done((response) => {
-            $('.borderdate-modal .modal-body').html(response.html);
-        })
+const borderdateModalElement = $('.borderdate-modal');
+borderdateModalElement.on('shown.bs.modal', (event) => {
+    $.ajax({
+        url: '/entity/modal/borderdate/' + event.target.dataset.type + '/' + event.target.dataset.id
+    }).done((response) => {
+        $('.borderdate-modal .modal-body').html(response.html);
     })
-
+});
+export function openBorderdateModal() {
     const borderdateModal = new bootstrap.Modal('.borderdate-modal', {});
     borderdateModal.show();
 }
--- resources/js/mergemodal.js
+++ resources/js/mergemodal.js
@@ -1,13 +1,13 @@
-export function openMergeModal() {
-    const mergeModalElement = $('.merge-modal');
-    mergeModalElement.on('shown.bs.modal', (event) => {
-        $.ajax({
-            url: '/entity/modal/merge/' + event.target.dataset.type + '/' + event.target.dataset.id
-        }).done((response) => {
-            $('.merge-modal .modal-body').html(response.html);
-        })
+const mergeModalElement = $('.merge-modal');
+mergeModalElement.on('shown.bs.modal', (event) => {
+    $.ajax({
+        url: '/entity/modal/merge/' + event.target.dataset.type + '/' + event.target.dataset.id
+    }).done((response) => {
+        $('.merge-modal .modal-body').html(response.html);
     })
+});
 
+export function openMergeModal() {
     const mergeModal = new bootstrap.Modal('.merge-modal', {});
     mergeModal.show();
 }
