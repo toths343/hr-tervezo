273c4c81e81d40eb12847ace3d5d5fae911c0169
tgabor343 <89416923+tgabor343@users.noreply.github.com>
2024-08-30 18:51:52 +0200

    modals

--- resources/js/borderdatemodal.js
+++ resources/js/borderdatemodal.js
@@ -1,21 +1,33 @@
-$('.btn-borderdate-modal-open').on('click.borderdate-modal-open', () => {
-    openBorderdateModal();
-});
+class BorderdateModal
+{
+    constructor() {
+        this.init();
+    }
 
-const borderdateModalElement = $('.borderdate-modal');
-borderdateModalElement.on('shown.bs.modal', (event) => {
-    $.ajax({
-        url: '/entity/modal/borderdate/' + event.target.dataset.type + '/' + event.target.dataset.id
-    }).done((response) => {
-        $('.borderdate-modal .modal-body').html(response.html);
-    })
-});
-function openBorderdateModal() {
-    const borderdateModal = new bootstrap.Modal('.borderdate-modal', {});
-    borderdateModal.show();
+    init() {
+        $('.btn-borderdate-modal-open').on('click.borderdate-modal-open', () => {
+            this.openBorderdateModal();
+        });
+
+        const borderdateModalElement = $('.borderdate-modal');
+        borderdateModalElement.on('shown.bs.modal', (event) => {
+            $.ajax({
+                url: '/entity/modal/borderdate/' + event.target.dataset.type + '/' + event.target.dataset.id
+            }).done((response) => {
+                $('.borderdate-modal .modal-body').html(response.html);
+            })
+        });
+    }
+
+    openBorderdateModal() {
+        const borderdateModal = new bootstrap.Modal('.borderdate-modal', {});
+        borderdateModal.show();
+    }
+
+    saveBorderdate() {
+        const borderdateModal = new bootstrap.Modal('.borderdate-modal', {});
+        borderdateModal.show();
+    }
 }
 
-function saveBorderdate() {
-    const borderdateModal = new bootstrap.Modal('.borderdate-modal', {});
-    borderdateModal.show();
-}
+new BorderdateModal();
--- resources/js/mergemodal.js
+++ resources/js/mergemodal.js
@@ -1,17 +1,28 @@
-$('.btn-merge-modal-open').on('click.merge-modal-open', () => {
-    openMergeModal();
-});
+class MergeModal
+{
+    constructor() {
+        this.init();
+    }
 
-const mergeModalElement = $('.merge-modal');
-mergeModalElement.on('shown.bs.modal', (event) => {
-    $.ajax({
-        url: '/entity/modal/merge/' + event.target.dataset.type + '/' + event.target.dataset.id
-    }).done((response) => {
-        $('.merge-modal .modal-body').html(response.html);
-    })
-});
+    init() {
+        $('.btn-merge-modal-open').on('click.merge-modal-open', () => {
+            this.openMergeModal();
+        });
 
-function openMergeModal() {
-    const mergeModal = new bootstrap.Modal('.merge-modal', {});
-    mergeModal.show();
+        const mergeModalElement = $('.merge-modal');
+        mergeModalElement.on('shown.bs.modal', (event) => {
+            $.ajax({
+                url: '/entity/modal/merge/' + event.target.dataset.type + '/' + event.target.dataset.id
+            }).done((response) => {
+                $('.merge-modal .modal-body').html(response.html);
+            })
+        });
+    }
+
+    openMergeModal() {
+        const mergeModal = new bootstrap.Modal('.merge-modal', {});
+        mergeModal.show();
+    }
 }
+
+new MergeModal();
